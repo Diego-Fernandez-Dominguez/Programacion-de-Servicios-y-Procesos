e3f24b462bc40179691fe26e8406a3b009da4235
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-09-24 10:25:12 +0200

    Exercise11 correction

--- Tema0/Exercise11.py
+++ Tema0/Exercise11.py
@@ -2,15 +2,18 @@ def exercise11(vocal1:chr):
 
     lista = ["a", "e", "i", "o", "u"]
 
-    if lista.__contains__(vocal1):
-        print("Good boy")
-    else:
-        print("Bad boy")
+    return lista.__contains__(vocal1)
+
 
 def main():
     vocal=input("sAY IT")
 
-    exercise11(vocal)
+    result=exercise11(vocal)
+
+    if result:
+        print("Good boy")
+    else:
+        print("Bad boy")
 
 if __name__ == "__main__":
     main()
