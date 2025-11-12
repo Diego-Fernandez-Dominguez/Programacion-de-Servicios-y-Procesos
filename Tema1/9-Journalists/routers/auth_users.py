aea7b2baeacd663d19d2a49ecff6d67db8139b9c
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-11-12 10:15:51 +0100

    Comparado a interfaces, esto es un paseo

--- Tema1/9-Journalists/routers/auth_users.py
+++ Tema1/9-Journalists/routers/auth_users.py
@@ -1,5 +1,6 @@
+from datetime import *
 from pydantic import BaseModel
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 
 import jwt
 from jwt.exceptions import InvalidTokenError
@@ -48,7 +49,16 @@ users_db = {
         "email": "correoprueba@gmail.com",
         "disable": False,
         "password": "$argon2id$v=19$m=65536,t=3,p=4$Yjhn/g3ouDCS/J2PdQQ2rw$gV41w60nuHqTYhgGMShhHrRZ9R8jutOfRiP63SGKCTs"
+    },
+    #Password: prueba
+    "pruebota2" : {
+        "username": "pruebota2",
+        "fullname": "pruebotalolazo2",
+        "email": "correoprueba2@gmail.com",
+        "disable": False,
+        "password": "$argon2id$v=19$m=65536,t=3,p=4$IbZy0fx6z7jC6L7V7mQywQ$TOPSLj6vM873h7uAUeB3dRFfhuI2lgsM1W5wuOamOwo"
     }
+
 }
 
 
@@ -61,3 +71,48 @@ def register(user: UserDB):
         return user
     else:
         raise HTTPException(status_code = 409, detail="User alredy exists")
+    
+@router.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+
+    user_db=users_db.get(form.username)
+
+    if users_db:
+        user=UserDB(**user_db)
+        try:
+            if password_hash.verify(form.password, user.password): 
+                expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRES_MINUTES)
+                access_token= {"sub":user.username, "exp":expire}
+
+                token = jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM)
+                return {"access_token" : token, "token_type" : "bearer"}
+            #raise HTTPException(status_code=401, detail="Contraseña incorrecta")
+        except: 
+            raise HTTPException(status_code=400, detail="Error al verificar la contraseña")
+    raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectas")
+
+@router.post("/", status_code=201, response_model=User)
+def add_user(user:User, authorized = Depends(auth_user)):
+    pass
+
+#Functions
+
+async def auth_user(token:str = Depends(oauth2)):
+    try:
+        username=jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM).get("sub")
+
+        if username is None:
+                raise HTTPException(status_code=401, 
+                                    detail="Credenciales de autenticacion invalidas",
+                                    headers={"WWW-Authenticate": "Bearer"})
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401,
+                            detail="Credenciales de autenticacion invalidas",
+                            headers={"WWW-Authenticate": "Bearer"})
+    
+    user = User(**users_db[username])
+    if user.disable:
+        raise HTTPException(status_code=400,
+                            detail="Usuario inactivo")
+    
+    return user
